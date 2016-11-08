36f0b3d2084caac306fe7680f6ca3f187df7f87a
ani24sri <ani24sri@gmail.com>
2016-11-08 22:33:25 +0530

    haah

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,29 @@
 console.log('This would be the main JS file.');
 
-function playMusic()
-{
-    var music =document.getElementById('music');
-    music.play();
-}
+              $(document).ready(function ()
+                               {
+                 $('#banner').append(new Date().getFullYear().toString()); 
+              });
+
+   $( function() {
+    $( "#intro" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        
+        effect: "explode",
+        duration: 1000
+      }
+    }).on('close',function(e){
+   var player = videojs('my-video');
+VideoJS.DOMReady(function(){
+  player.pause();  
+});
+        });
+    $( "#vid" ).on( "click", function() {
+      $( "#intro" ).dialog( "open" );
+    });
+  } );
